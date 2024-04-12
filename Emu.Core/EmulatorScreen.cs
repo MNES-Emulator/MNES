328d0255a98faa3909c69cb577530d70b5681d60
Jamie Cappel <jamieyello@yahoo.com>
2024-04-11 21:53:29 -0400

    Add missing projects to main sln

--- Emu.Core/EmulatorScreen.cs
+++ Emu.Core/EmulatorScreen.cs
@@ -3,6 +3,6 @@
 public abstract class EmulatorScreen { 
    public abstract int Width { get; }
    public abstract int Height { get; }
-   /// <summary> Rgba8: red, green, blue, alpha, with a bit depth of 8. </summary>
+   /// <summary> Rgba8: red green blue alpha with a bit depth of 8. </summary>
    public abstract byte[] Buffer { get; }
 }
