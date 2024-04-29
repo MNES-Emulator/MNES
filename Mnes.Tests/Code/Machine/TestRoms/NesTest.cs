395b72eebf5fb59c7997d36ff2db12b4882129bf
Jamie Cappel <jamieyello@yahoo.com>
2024-04-29 06:18:46 -0400

    More improvements to nestest

--- Mnes.Tests/Code/Machine/TestRoms/NesTest.cs
+++ Mnes.Tests/Code/Machine/TestRoms/NesTest.cs
@@ -30,11 +30,12 @@ public sealed class NesTest {
    // nestest writes to certain areas of memory the results of how accurately it was run,
    // so evaluating those addresses at the end, as well as making sure our log is identical
    // to the Nintendulator one will determine whether this test passes or not.
-   [TestMethod/*, Ignore*/]
+   [TestMethod, Ignore]
    public async Task RunNestest() {
       var input = new NesInputState();
       var settings = new MnesConfig();
       settings.System.DebugMode = true;
+      settings.System.DebugShowStatusFlags = true;
       // nestest has a custom entry point that will go through all the CPU instructions.
       // the log starts there, so we will too.
       settings.System.StartExecutionAtAddress = 0xC000;
@@ -48,12 +49,53 @@ public sealed class NesTest {
       void cpu_callback(CpuInstruction instruction) {
          var valid_log = valid_logs[i++];
          var current_log = machine.Logger.GetLast();
-         Debug.WriteLine($"     {valid_log} (control)");
+         Debug.WriteLine($"     {valid_log.GetDebugString(true)} (control)");
 
          Assert.AreEqual(
             valid_log.Instruction.OpCode,
             current_log.Instruction.OpCode,
             $"Opcode mismatch at instruction {i}");
+
+         Assert.AreEqual(
+            valid_log.Param1,
+            current_log.Param1,
+            $"Argument 1 mismatch at instruction {i}");
+
+         Assert.AreEqual(
+            valid_log.Param2,
+            current_log.Param2,
+            $"Argument 2 mismatch at instruction {i}");
+
+         // This should only be a warning for now at most
+         //Assert.AreEqual(
+         //   valid_log.Message,
+         //   current_log.Message,
+         //   $"Argument 1 mismatch at instruction {i}");
+
+         Assert.AreEqual(
+            valid_log.CpuRegisters.A,
+            current_log.CpuRegisters.A,
+            $"CPU register A mismatch at instruction {i}");
+
+         Assert.AreEqual(
+            valid_log.CpuRegisters.X,
+            current_log.CpuRegisters.X,
+            $"CPU register X mismatch at instruction {i}");
+
+         Assert.AreEqual(
+            valid_log.CpuRegisters.Y,
+            current_log.CpuRegisters.Y,
+            $"CPU register Y mismatch at instruction {i}");
+
+         Assert.AreEqual(
+            valid_log.CpuRegisters.P,
+            current_log.CpuRegisters.P,
+            $"CPU register P mismatch at instruction {i}");
+
+         Assert.AreEqual(
+            valid_log.CpuRegisters.S,
+            current_log.CpuRegisters.S,
+            $"CPU register SP mismatch at instruction {i}");
       }
 
       machine.Callbacks.OnNesInstructionExecute += cpu_callback;
