a65953de3e6f3b66d2191095da748da7ced27e1c
Jamie Cappel <jamieyello@yahoo.com>
2024-05-01 04:19:34 -0400

    Clean up test code

--- Mnes.Tests/Code/Machine/TestRoms/NesTest.cs
+++ Mnes.Tests/Code/Machine/TestRoms/NesTest.cs
@@ -19,7 +19,7 @@ public sealed class NesTest {
 
    [TestMethod]
    public void Test_LogPathExists() =>
-   JamieAssert.FileExists(NESTEST_LOG_PATH);
+      JamieAssert.FileExists(NESTEST_LOG_PATH);
 
    // nestest writes to certain areas of memory the results of how accurately it was run,
    // so evaluating those addresses at the end, as well as making sure our log is identical
@@ -29,17 +29,16 @@ public sealed class NesTest {
    // to see if illegal opcodes are working, and none of those are implemented atm.
    [TestMethod]
    public async Task RunNestest() {
-      var input = new NesInputState();
-      var settings = new MnesConfig();
-      settings.System.DebugMode = true;
-      settings.System.DebugShowStatusFlags = true;
-      // nestest has a custom entry point that will go through all the CPU instructions.
-      // the log starts there, so we will too.
-      settings.System.StartExecutionAtAddress = 0xC000;
       var rom_data = File.ReadAllBytes(NESTEST_PATH);
       var valid_logs = File.ReadAllLines(NESTEST_LOG_PATH)
          .Select(x => InstructionLog.FromString(x))
          .ToArray();
+
+      var input = new NesInputState();
+      var settings = new MnesConfig();
+      settings.System.DebugMode = true;
+      settings.System.DebugShowStatusFlags = true;
+      settings.System.StartExecutionAtAddress = 0xC000; // test starts here
       var machine = new MachineState(rom_data, settings, input);
 
       int i = 0;
@@ -49,11 +48,12 @@ public sealed class NesTest {
          Debug.WriteLine($"     {valid_log.GetDebugString(true)} (control)");
          valid_log.AssertAreEqual(current_log, i);
          Debug.WriteLine("");
-         if (machine.Cpu.Registers.PC == 0xC6BC)
+         if (machine.Cpu.Registers.PC == 0xC6BC) // test ends here
             _ = machine.Stop();
       }
 
       machine.Callbacks.OnNesInstructionExecute += cpu_callback;
       await machine.Run();
+      // Todo: Check addresses that nestest writes results to.
    }
 }
